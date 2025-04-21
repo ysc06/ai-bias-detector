0968a5a80ce5ad02e035ce76a976b45d1a7d710e
Yu-Shan (Naomi) Cheng <104955815+ysc06@users.noreply.github.com>
2025-04-20 20:29:10 -0400

    Update ai-detector-for-bias-in-test-item.py

--- ai-detector-for-bias-in-test-item.py
+++ ai-detector-for-bias-in-test-item.py
@@ -1,6 +1,5 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-!pip install -q langgraph
 from langgraph.graph import StateGraph, END
 from langchain_core.tools import tool
 , END
